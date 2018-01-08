51ffee8dafc36f85792d11fb6a61bc14c80bb6cf
obiwan31415 <obiwan314@gmail.com>
2018-01-08 23:12:24 +0100

    First commit. Add bio object, literal notation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,30 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+ //$("#main").append("Rafał Gawlik");
+ var email = "obiwan1702@wp.pl";
+ var newEmail = email.replace("wp.pl", "outlook.com");
+ var name = "Rafał Gawlik";
+ var role = "Web Developer";
+ var bio ={
+ 	"firstName" : "Rafał",
+ 	"lastName"	: "Gawlik",
+ 	"name"		: "Rafał Gawlik",
+ 	"age"		: "49",
+ 	"role"		: "Web Developer",
+ 	"contact"	: "obiwan314@gmail.com",
+ 	"imageURL"	: "images/cat.jpg",
+ 	"welcomeMsg": "Live long and prosper",
+ 	"skills"	: ["awesomness", "programming", "teaching", "JS"]
+ };
+
+ console.log(newEmail);
+ var formattedName = HTMLheaderName.replace("%data%", bio.name);
+ var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+ var formattedImage = HTMLprojectImage.replace("%data%", bio.imageURL);
+ //$("#header").append(formattedName);
+ //$("#header").append(formattedRole);
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+ $("#header").prepend(formattedImage);
