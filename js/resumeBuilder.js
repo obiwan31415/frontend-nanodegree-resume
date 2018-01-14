a2c4ed15a05cfcce80d0773b367c31badc11f39f
obiwan31415 <obiwan314@gmail.com>
2018-01-14 15:17:57 +0100

    Add skills to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -22,26 +22,29 @@ var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 var formattedBioPic = HTMLprojectImage.replace("%data%", bio.bioPic);
-var formattedHTMLskills = [];
-for(i in bio.skills) {
-	HTMLskills.replace("%data%", "dupa");	
-};
 
 var formattedContactInfo = [];
 formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
 formattedContactInfo.push(HTMLmobile.replace("%data%", bio.contacts.mobile));
 formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
 formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+
+var formattedHTMLskills = [];
+
+bio.skills.forEach(function(element) {
+	formattedHTMLskills.push(HTMLskills.replace("%data%", element));
+});
  
 for(i in formattedContactInfo) {
 	$("#topContacts").append(formattedContactInfo[i]);
 };
+
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 $("#header").append(formattedBioPic);
 $("#header").append(formattedHTMLwelcomeMsg);
 
 $("#header").append(HTMLskillsStart);
-$("#skills").append('<li class="flex-item"><span class="white-text">dupa</span></li>');
-$("#skills").append('<li class="flex-item"><span class="white-text">dupa2</span></li>')
-
+formattedHTMLskills.forEach(function(element) {
+	$("#skills").append(element);
+});
